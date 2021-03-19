8f593f71869efbb4803e2e7722d29bb65e5c88db
Valdismin <Valdismin@gmail.com>
2021-03-19 15:30:16 +0300

    Updates

---
+++ precache-manifest.271604a71689e3d78d900887aa2cbe9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daa5ae2d7b7b2ed10c35c1c50b27a78a",
+    "url": "/Ignat_DZ/index.html"
+  },
+  {
+    "revision": "c58cf8b42d97e8606806",
+    "url": "/Ignat_DZ/static/css/main.529c18b1.chunk.css"
+  },
+  {
+    "revision": "4805a56ac9c4b25b5906",
+    "url": "/Ignat_DZ/static/js/2.730e08bc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Ignat_DZ/static/js/2.730e08bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c58cf8b42d97e8606806",
+    "url": "/Ignat_DZ/static/js/main.9e62b6c2.chunk.js"
+  },
+  {
+    "revision": "d6b7832bb68c5eaac06d",
+    "url": "/Ignat_DZ/static/js/runtime-main.7649001c.js"
+  },
+  {
+    "revision": "9064981b80e59f1b663c17fab41563c9",
+    "url": "/Ignat_DZ/static/media/Spinner-1s-200px.9064981b.svg"
+  }
+]);
